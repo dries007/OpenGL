444d41eb01144877cd5f85e202541dbf6f8f94e0
Dries007 <admin@dries007.net>
2018-02-12 22:24:54 +0100

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+OpenGL Course Work 2018
+=======================
+
+No copying!
---
+++ test/test.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <GL/glut.h>
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glColor3f(1, 0, 0);
+
+    glBegin(GL_LINES);
+    glVertex2i(180, 15);
+    glVertex2i(10, 145);
+    glEnd();
+
+    glColor3f(0, 1, 0);
+    glRecti(115, 80, 165, 140);
+
+    glColor3f(0, 0, 1);
+    glRasterPos2d(150, 20);
+    glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'L');
+    glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'i');
+    glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'j');
+    glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'n');
+
+    glFlush();
+}
+
+void reshape(int w, int h)
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 200, 0, 150);
+
+    glViewport(0, 0, w, h);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400, 300);
+    glutCreateWindow("Test oefening theorie");
+
+    glClearColor(1, 1, 1, 1);
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+
+    glutMainLoop();
+
+    return 0;
+}
